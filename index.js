d94a69959b4de1f82b17b2caa25ca8bad218eeb5
VincentCSS <vincenthtml@gmail.com>
2020-03-26 19:26:39 -0600

    refactored if statements and fixed all eslint errors.

--- index.js
+++ index.js
@@ -10,27 +10,25 @@ client.on('message', (msg) => {
   const queryMessage = msg.content.split(' ');
 
   if (queryMessage[0] === '!a' || queryMessage[0] === '!array') {
-    return arrMethods.search(queryMessage[1])
+    arrMethods.search(queryMessage[1])
       .then((res) => res.toJSON())
       .then((data) => msg.channel.send(data.url))
       .catch(() => msg.channel.send(`Unable to find an array method called ${queryMessage[1]}.`));
   }
 
   if (queryMessage[0] === '!s' || queryMessage[0] === '!string') {
-    return strMethods.search(queryMessage[1])
+    strMethods.search(queryMessage[1])
       .then((res) => res.toJSON())
       .then((data) => msg.channel.send(data.url))
-      .catch(() => msg.channel.send(`Unable to find an string method called ${queryMessage[1]}.`));
+      .catch(() => msg.channel.send(`Unable to find a string method called ${queryMessage[1]}.`));
   }
 
   if (queryMessage[0] === '!o' || queryMessage[0] === '!object') {
-    return objMethods.search(queryMessage[1])
+    objMethods.search(queryMessage[1])
       .then((res) => res.toJSON())
       .then((data) => msg.channel.send(data.url))
       .catch(() => msg.channel.send(`Unable to find an object method called ${queryMessage[1]}.`));
   }
-
-  return true;
 });
 
 client.login(process.env.BOT_TOKEN);
